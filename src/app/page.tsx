1fbae22364a487fc6699ae4ebfe8e38674fd3dfb
dhruvshetty0103 <dhruvshetty0103@gmail.com>
2025-01-06 15:44:07 +0530

    Regex to load images in web

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,53 @@
-import { NAME } from '@/constants/CMSEntryIds';
+import { HOME } from '@/constants/CMSEntryIds';
 import { fetchDataByCMSEntryID } from '@/lib/contentful';
+import Image from 'next/image';
+
+const parseMarkdown = (markdown:string) => {
+  const parts = markdown.split(/(!\[.*?\]\(.*?\))/); // Split by Markdown image syntax
+  return parts.map((part) => {
+    const imageRegex = /!\[(.*?)\]\((.*?)\)/;
+    const match = RegExp(imageRegex).exec(part);
+    if (match) {
+      let imageUrl = match[2];
+      // Ensure the URL is absolute
+      if (imageUrl.startsWith('//')) {
+        imageUrl = `https:${imageUrl}`;
+      }
+      return {
+        type: 'image',
+        alt: match[1],
+        url: imageUrl,
+      };
+    }
+    return { type: 'text', content: part.trim() }; // Non-image content
+  });
+};
 
 export default async function Home() {
-  const data = await fetchDataByCMSEntryID(NAME)
+  const homePageData = await fetchDataByCMSEntryID(HOME);
+
+  const parsedContent = parseMarkdown(homePageData.description as string);
+  
   
   return (
     <main className="h-screen">
-      <p>{data.name as string}</p>
+      {parsedContent.map((item) => {
+        if (item.type === 'image' && item.url) {
+          return (
+            <Image
+              key={`index-${item.url}`}
+              src={item.url}
+              width={180}
+              height={180}
+              alt={item.alt ?? 'Image'}
+            />
+          );
+        }
+        if (item.type === 'text' && item.content) {
+          return <h1 key={`index-${item.content}`}>{item.content}</h1>;
+        }
+        return null;
+      })}
     </main>
   );
 }
